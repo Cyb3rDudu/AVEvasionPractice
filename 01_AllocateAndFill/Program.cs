bd7bdfa5572df416205c34919cc7c8e49f0fbe0f
Cyb3rDudu <jamu85@icloud.com>
2024-07-08 22:43:59 +0200

    Adds Allocate and Fill Project

---
+++ 01_AllocateAndFill/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_AllocateAndFill
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte zeroVal = 1;
+            byte[] data = new byte[32768 * 32768];
+            Array.Clear(data, 0, data.Length);
+            Console.WriteLine("~1GB filled!");
+            System.Threading.Thread.Sleep(1000);
+            byte lastVal = (byte)data.GetValue((32768 * 32768) - 1);
+            if (lastVal.Equals(zeroVal))
+            {
+                runner();
+            }
+        }
+        static void runner()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
